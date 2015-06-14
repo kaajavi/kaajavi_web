743ae198af9dfb11c88d40006c42b79bf3a90721
kaajavi <javierguignard@gmail.com>
2015-06-14 20:35:56 -0300

    add tmplates

--- webblog/urls.py
+++ webblog/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from webblog import settings
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^', include('blog.urls', namespace = "app_blog")),
     url(r'^admin/', include(admin.site.urls)),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
